c0f9f8fba1821929a68102305d9c4813cf8102a3
DiliniSew <dilinirpd@gmail.com>
2024-05-17 15:32:21 +0530

    update admin

--- src/app/signUpComponents/admin.tsx
+++ src/app/signUpComponents/admin.tsx
@@ -21,22 +21,22 @@ export function Admin() {
           <div className="grid grid-cols-2 gap-4">
             <div className="grid gap-2">
               <Label htmlFor="first-name">First name</Label>
-              <Input id="first-name" required />
+              <Input id="first-name" className="hover:cursor-pointer" required />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="last-name">Last name</Label>
-              <Input id="last-name" required />
+              <Input id="last-name" className="hover:cursor-pointer" required />
             </div>
           </div>
 
           <div className="grid grid-cols-2 gap-4">
             <div className="grid gap-2">
               <Label htmlFor="first-name">User Name</Label>
-              <Input id="user-name" required />
+              <Input id="user-name" className="hover:cursor-pointer" required />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="last-name">Mobile Number</Label>
-              <Input id="mobile" required />
+              <Input id="mobile" className="hover:cursor-pointer" required />
             </div>
           </div>
 
@@ -46,19 +46,20 @@ export function Admin() {
               id="email"
               type="email"
               placeholder="m@example.com"
+              className="hover:cursor-pointer"
               required
             />
           </div>
 
           <div className="grid gap-2">
             <Label htmlFor="admin-type">Admin Type</Label>
-            <Input id="admin-type" required />
+            <Input id="admin-type" className="hover:cursor-pointer" required />
           </div>
 
           <div className="grid grid-cols-2 gap-4">
             <div className="grid gap-2">
               <Label htmlFor="first-name">Password</Label>
-              <Input id="password" type="password" required />
+              <Input id="password" type="password" className="hover:cursor-pointer" required />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="last-name">Confirm Password</Label>
@@ -68,12 +69,13 @@ export function Admin() {
                 minLength={8}
                 maxLength={20}
                 pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,20}$"
+                className="hover:cursor-pointer"
                 required
               />
             </div>
           </div>
 
-          <Button type="submit" className="w-full">
+          <Button type="submit" className="w-full hover:bg-red-500">
             Update Admin
           </Button>
         </div>
