de8345c497ec42bc7f9034ab56a05186c7354a3d
DiliniSew <dilinirpd@gmail.com>
2024-05-17 15:09:13 +0530

    Add admin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import LoginForm from "./login/page";
 import SignUpPage from "./signup/page";
+import { Admin } from "./signUpComponents/admin";
 
 export default function Home() {
   return (
     //<LoginForm/>
-    <SignUpPage/>
+    <Admin/>
   )
     
 }
